a3f04c1aa3b6fb8666ac76798c5623183ffa669d
skhoct30@naver.com <>
2025-06-25 17:02:21 +0900

    [서강현]
    - 로그인을 위한 controller / html 작성중 #1
    - 회원가입을 위한 controller / html 작성중 #2

---
+++ src/main/java/com/skhoct30/sns/user/UserController.java
@@ -0,0 +1,23 @@
+package com.skhoct30.sns.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+	
+	
+	@GetMapping("/join-view")
+	public String joinInput() {
+		return "user/join";
+	}
+	
+	@GetMapping("/login-view")
+	public String loginInput() {
+		return "user/login";
+	}
+	
+}
